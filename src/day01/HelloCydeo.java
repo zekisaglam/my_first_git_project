02d2451fad2a61a3b3a15d3c0706781b07f21d5d
Zeki Saglam <64930322+zekisaglam@users.noreply.github.com>
2025-11-06 16:14:36 -0500

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,5 +4,8 @@ public class HelloCydeo {
     public static void main(String[] args) {
 
         System.out.println("Hello Cydeo");
+         System.out.println("Hello C");
+         System.out.println("Hello Cy");
+    
     }
 }
